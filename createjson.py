b8b33b6df98b3f071db97e23c641912a0d82ba7a
Simon <coopersimon@gmail.com>
2020-10-11 22:27:00 +0100

    database

--- createjson.py
+++ createjson.py
@@ -33,7 +33,7 @@ def stations():
             else:
                 print(row[0])
                 key = row[1].replace('.',',')
-                stations.append({key:{'historic-county':row[3],'long':row[5],'lat':row[4],'elevation':row[6],'start-year':row[7],'end-year':row[8]}})
+                stations.append({'station_name':row[1],'station_file_name':row[2],'historic-county':row[3],'long':float(row[5]),'lat':float(row[4]),'elevation':float(row[6]),'start-year':int(row[7]),'end-year':int(row[8])})
 
 
     c = MongoClient()
@@ -45,64 +45,64 @@ def stations():
 
 
 #for each site record timestamp, max air temp, min air temp, min grass temp, 
-
-root = '/home/simon/Downloads/uk-daily-temperature-obs/ftp.ceda.ac.uk/badc/ukmo-midas-open/data/uk-daily-temperature-obs/'
-filesIncPath = []
-for path, subdirs, files in os.walk(root):
-    for name in files:
-        if '/qc-version-0' in path:
-            continue
-        if name.endswith('capability.csv') or name.endswith('_change_log.txt') or name.endswith('README_catalogue_and_licence.txt') or name.startswith('.') or name.endswith('_station-metadata.csv'):
-            continue
-        #print(os.path.join(path, name))
-        filesIncPath.append(os.path.join(path, name))
-
-
-obsv = []
-
-for f in filesIncPath:
-    with open(f, newline='') as csvfile:
-        areader = csv.reader(csvfile, delimiter=',',doublequote=False)
-        dataMarkerFound = False
-        dataColumnHeaderFound = False
-        stationName = "undefined"
-        print(f)
-        for row in areader:            
-            if row[0] == 'data':
-                dataMarkerFound = True
+def observations():
+    root = '/home/simon/Downloads/uk-daily-temperature-obs/ftp.ceda.ac.uk/badc/ukmo-midas-open/data/uk-daily-temperature-obs/'
+    filesIncPath = []
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            if '/qc-version-0' in path:
                 continue
-            elif row[0] == 'observation_station':
-                #station['observation_station'] = row[2]
-                #station['data'] = []
-                stationName = row[2]
+            if name.endswith('capability.csv') or name.endswith('_change_log.txt') or name.endswith('README_catalogue_and_licence.txt') or name.startswith('.') or name.endswith('_station-metadata.csv'):
                 continue
-            elif row[0] == 'end data':#end of data reached
-                continue
-            elif row[0] == 'ob_end_time' and dataMarkerFound:
-                dataColumnHeaderFound = True
-                continue
-            elif dataMarkerFound and dataColumnHeaderFound:
-                station = {} 
-                station['observation_station'] = stationName
-                station['ob_end_time'] = datetime.fromisoformat(row[0])
-                station['ob_hour_count'] = float(row[3]) if row[3]!=  'NA' else None
-                station['max_air_temp'] = row[8]
-                station['min_air_temp'] = row[9]
-                station['min_grss_temp'] = row[10]
-                station['min_conc_temp'] = row[11]
-                obsv.append(station)
-                
-                                    
-        #print(obsv)
-        #break #temp
-
-c = MongoClient()
-db = c.test
-
-db.observations.insert_many(obsv)
-    
+            #print(os.path.join(path, name))
+            filesIncPath.append(os.path.join(path, name))
 
 
+    obsv = []
+
+    for f in filesIncPath:
+        with open(f, newline='') as csvfile:
+            areader = csv.reader(csvfile, delimiter=',',doublequote=False)
+            dataMarkerFound = False
+            dataColumnHeaderFound = False
+            stationName = "undefined"
+            print(f)
+            for row in areader:            
+                if row[0] == 'data':
+                    dataMarkerFound = True
+                    continue
+                elif row[0] == 'observation_station':
+                    #station['observation_station'] = row[2]
+                    #station['data'] = []
+                    stationName = row[2]
+                    continue
+                elif row[0] == 'end data':#end of data reached
+                    continue
+                elif row[0] == 'ob_end_time' and dataMarkerFound:
+                    dataColumnHeaderFound = True
+                    continue
+                elif dataMarkerFound and dataColumnHeaderFound:
+                    station = {} 
+                    station['observation_station'] = stationName
+                    station['ob_end_time'] = datetime.fromisoformat(row[0])
+                    station['ob_hour_count'] = float(row[3]) if row[3]!=  'NA' else None
+                    station['max_air_temp'] = float(row[8]) if row[8]!= 'NA' else None
+                    station['min_air_temp'] = float(row[9]) if row[9]!='NA' else None
+                    station['min_grss_temp'] = float(row[10]) if row[10]!='NA' else None
+                    station['min_conc_temp'] = float(row[11]) if row[11]!='NA' else None
+                    obsv.append(station)
+                    
+                                        
+            #print(obsv)
+            #break #temp
+
+    c = MongoClient()
+    db = c.test
+
+    db.observations.insert_many(obsv)
+        
 
 
 
+
+#station()
---
+++ db.js
@@ -0,0 +1,78 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost/test';
+
+const client = new MongoClient(url,{ useUnifiedTopology: true });
+
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db('test');
+
+    //findObservations(db,function(){
+        GetFirstYear(db,"",function(){
+            client.close();
+        });
+    //});
+
+});
+
+const findObservations = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('observations');
+  // Find some documents
+    collection.find({"min_air_temp":{"$lte":0},"observation_station":"berriedale-langwell"}).sort({"ob_end_time":1}).toArray(function(err, docs) {
+    //assert.equal(err, null);
+    //console.log("Found the following records");
+    //console.log(docs)
+    callback(docs);
+  });
+}
+
+const GetFirstYear = function(db, station, callback){
+    const collection = db.collection('observations');
+
+    collection.find({"min_air_temp":{"$lte":0},"observation_station":"berriedale-langwell"}).sort({"ob_end_time":1}).next(function(err, docs){
+        var startDate = docs['ob_end_time'];        
+
+        console.log(startDate);
+        console.log(startDate.getMonth());
+        callback(docs)    
+    })
+    
+}
+
+//db.observations.find({"min_air_temp":{"$lte":0},"observation_station":"berriedale-langwell"}).sort({"ob_end_time":1})
+
+// Use connect method to connect to the Server
+/*MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
+    const db = client.db("test");
+
+
+    console.log("connected successfully to server");
+
+   // db.collection('inventory').find().then(function(result){
+   //     console.log(result);
+   // })
+    //
+    const collection = db.collection('observations');
+    collection.find({}).toArray(function(err, result){
+        assert.equal(null,err);
+        console.log("found the following records");
+        console.log(results);
+
+    });
+
+
+    assert.equal(null, err);
+    client.close();
+});*/
+
+
+
+
+
+
