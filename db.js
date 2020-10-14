889582cce64e37c339fb5f1678c663b2f4c4b272
Simon <coopersimon@gmail.com>
2020-10-14 03:22:04 +0100

    database

--- db.js
+++ db.js
@@ -14,8 +14,10 @@ client.connect(function(err) {
 
     //findObservations(db,function(){
     GetFirstYear(db,"",function(results){
-        LastYear(db,"",results['startdate'],function(){
-            client.close();
+        LastYear(db,"",results,function(){
+            CreateYearArray(db,"",results,function(){
+                client.close();
+            });
         });
     });
 
@@ -39,7 +41,7 @@ const GetFirstYear = function(db, station, callback){
     collection.find({"min_air_temp":{"$lte":0},"observation_station":"berriedale-langwell"}).sort({"ob_end_time":1}).next(function(err, docs){
         let startDate = docs['ob_end_time'];                        
         console.log(`the first date is ${startDate.toJSON()}`);
-        callback(startDate)    
+        callback({"startDate":startDate})    
     });
     
 }
@@ -50,16 +52,53 @@ const LastYear = function(db,station,results,callback){
     collection.find({"min_air_temp":{"$lte":0},"observation_station":"berriedale-langwell"}).sort({"ob_end_time":-1}).next(function(err, docs){
         let endDate  = docs['ob_end_time'];                        
         console.log(`the last date is ${endDate.toJSON()}`);
-        callback(endDate);   
+        results['endDate'] = endDate;
+        callback(results);   
     });
 };
 
-const createYearArray = function(db,station,results,callback){
+const GetStationNames = function(db,callback){
+    const collection = db.collection('stations');
+    collection.find().project({"station_name" :1}).each(function(err,doc){
+        if(doc){
+        } else{
+            //each will keep getting docs until none left of query, and then
+            //this else block is run.
+        }
+
+    });
 }
 
-const createFirstLastFrostDictionaryArray = function(){
+const CreateYearArray = function(db,station,results,callback){
+    //we are looking for full years 1-jul-year1/30-jun-year2
+    //that way the first and last fost is calculated from this range
+    //this makes sense because it covers autumn,winter,spring, the seasons
+    //where relavent data can happen.
+    let startDate = results['startDate'];
+    let endDate = results['endDate'];
+    let startYear = startDate.getFullYear();
+    if (startDate.getMonth() >= 6){
+        //we move the start year forward one
+        startYear += 1;
+    }
+    let endYear = endDate.getFullYear();
+    if (endDate.getMonth() <6){
+        endYear -=1;
+    }
+    let theYears = range(endYear-startYear,startYear);
+    callback({"years":theYears});
 }
 
+const createFirstLastFrostDictionaryArray = function(db,station results,callback){
+
+
+}
+
+
+
+function range(size, startAt = 0) {
+        return [...Array(size).keys()].map(i => i + startAt);
+}
 
 //db.observations.find({"min_air_temp":{"$lte":0},"observation_station":"berriedale-langwell"}).sort({"ob_end_time":1})
 
